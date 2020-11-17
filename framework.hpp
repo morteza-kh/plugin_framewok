b54e8d1c5ccd119a33e8597a0bc0ecc0d2f68eab
morteza-kh <morteza.kh.r@gmail.com>
2020-11-17 23:53:56 +0330

    some changes

--- framework.hpp
+++ framework.hpp
@@ -7,42 +7,39 @@
 #include <boost/asio.hpp>
 #include <boost/signals2.hpp>
 
-//template <typename... Args>
-//class event {
-//	boost::signals2::signal<void(Args...)> sig_;
-//
-//public:
-//	event() {}
-//	virtual ~event() = default;
-//
-//	template <typename F>
-//	auto connect(F &&f) -> void {
-//		sig_.connect(std::move(f));
-//	}
-//
-//	auto operator()(Args &&...args) -> void { sig_(args...); }
-//};
-
 template <typename... Args>
-using event = boost::signals2::signal<void(Args...)>;
+class event {
+	boost::signals2::signal<void(Args...)> sig_;
 
-class handler {
+public:
+	event() {}
+	virtual ~event() = default;
+
+	template <typename F>
+	auto connect(F &&f) -> void {
+		sig_.connect(std::move(f));
+	}
+
+	auto operator()(Args &&...args) -> void { sig_(args...); }
+};
+
+class plugin {
 	std::unique_ptr<boost::asio::io_context> io_;
 	boost::asio::executor_work_guard<boost::asio::io_context::executor_type> work_;
 
 public:
-	handler()
+	plugin()
 		: io_ { new boost::asio::io_context {} }
 		, work_ { boost::asio::make_work_guard(*io_) } {
 		(std::thread { [this] { io_->run(); } }).detach();
 	}
 
 	template <typename... Args, typename F>
-	auto handle(event<Args...> &e, F &&f) -> void {
+	auto operator()(event<Args...> &e, F &&f) -> void {
 		e.connect([this, f = std::forward<F>(f)](Args... args) { io_->post(std::bind(f, args...)); });
 	}
 
-	virtual ~handler() = default;
+	virtual ~plugin() = default;
 };
 
 class framework {
